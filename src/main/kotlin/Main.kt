07755d9d26290f51c970060e1f151272c36a3e19
jedlimlx <jedlimlx@outlook.com>
2022-09-04 20:12:52 +0800

    Add menu bar (file loading and stuff doesn't work though)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,9 +4,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyShortcut
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.MenuBar
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
@@ -14,6 +18,7 @@ import backend.Video
 import gui.NodesPane
 import gui.VideoPlayer
 
+@OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     val video = Video("C:\\Users\\jedli\\OneDrive - NUS High School\\Documents\\Physics\\SYPT 2022\\" +
             "2. Rayleigh Disk\\Experimental Data\\Intensity Data 1\\IMG_3121.MOV")
@@ -27,6 +32,13 @@ fun main() {
             title = "Tracker 2.0",
             state = rememberWindowState(width = 320.dp, height = 300.dp)
         ) {
+            MenuBar { // TODO Add actual functionality to menu bar
+                Menu("File", mnemonic = 'F') {
+                    Item("Open File", onClick = { }, shortcut = KeyShortcut(Key.O, ctrl = true))
+                    Item("Save File", onClick = { }, shortcut = KeyShortcut(Key.S, ctrl = true))
+                }
+            }
+
             MaterialTheme {
                 Row(modifier = Modifier.padding(10.dp)) {
                     VideoPlayer(video)
